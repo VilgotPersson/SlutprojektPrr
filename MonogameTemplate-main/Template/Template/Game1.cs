1f82157e847e07cac1db234a53d804542e790eac
VilgotPersson <69978046+VilgotPersson@users.noreply.github.com>
2021-06-03 20:39:03 +0200

    Lade till rören

--- MonogameTemplate-main/Template/Template/Game1.cs
+++ MonogameTemplate-main/Template/Template/Game1.cs
@@ -12,8 +12,12 @@ namespace Template
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D Flappy1;
+        Texture2D pipe;
+        Vector2 pipePos = new Vector2(1900, 600);
         Vector2 Flappy1Pos = new Vector2(960, 540);
         Vector2 snabbhet;
+        float hopp = -1;
+        bool play = true;
         //KOmentar
         public Game1()
         {
@@ -44,6 +48,7 @@ namespace Template
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             Flappy1 = Content.Load<Texture2D>("Flappy1");
+            pipe = Content.Load<Texture2D>("pipe");
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here 
@@ -65,26 +70,34 @@ namespace Template
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            Flappy1Pos += snabbhet;
-            if (Keyboard.GetState().IsKeyDown(Keys.Space)) 
+            while (play == true)
             {
-                Flappy1Pos.Y -= 2f;
-                snabbhet.Y =  -2f;
+                Flappy1Pos += snabbhet;
+                if (Keyboard.GetState().IsKeyDown(Keys.Space))
+                {
+                    Flappy1Pos.Y -= 4f;
+                    snabbhet.Y = -4f;
+                    hopp -= 0.02f;
+                }
+                int i = 1;
+                snabbhet.Y += 0.25f * i;
+
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                {
+                    Exit();
+                }
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                Rectangle playerRect = new Rectangle((int)Flappy1Pos.X, (int)Flappy1Pos.Y, Flappy1.Width, Flappy1.Height);
+                Rectangle pipeRect = new Rectangle((int)pipePos.X, (int)pipePos.Y, pipe.Width, pipe.Height);
+          
+            if(playerRect.Intersects(pipeRect))
             {
-                Flappy1Pos.Y -= 2f;
-                snabbhet.Y = -2f;
+                Flappy1Pos.X = 500;
+                Flappy1Pos.Y = 500;
+                play = false;
             }
-            int i = 1;
-                snabbhet.Y += 0.15f * i;
-
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-            
-
             // TODO: Add your update logic here
-
+            pipePos.X += hopp;
             base.Update(gameTime);
         }
 
@@ -97,10 +110,10 @@ namespace Template
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here.
-            spriteBatch.Begin();
-            spriteBatch.Draw(Flappy1, Flappy1Pos, Color.White);
-            spriteBatch.End();
-
+                spriteBatch.Begin();
+                spriteBatch.Draw(Flappy1, Flappy1Pos, Color.White);
+                spriteBatch.Draw(pipe, pipePos, Color.White);
+                spriteBatch.End();
             base.Draw(gameTime);
         }
     }
